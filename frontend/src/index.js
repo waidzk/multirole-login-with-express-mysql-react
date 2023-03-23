b4c2cde2b75177e07aedaadcc8235fab47941419
waidzk <waidzk30@gmail.com>
2023-03-23 13:34:05 +0700

    add frontend side (react-redux, authSlice, asyncThunk, get user login, logout, protected all page)

---
+++ frontend/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import App from './App';
+import "bulma/css/bulma.css";
+import axios from "axios";
+
+// agar setiap request yang kita lakukan ke server selalu menyertekan credential nya
+// karena ini berlaku secara global
+axios.defaults.withCredentials = true;
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
